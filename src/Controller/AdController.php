45decadac21fa191addbf4a614fa0d90e58f8e94
Marillier Corentin <corentin@MacBook-Pro-de-Marillier.local>
2020-05-06 17:09:15 +0200

    Gestion des annonces, formulaire de création et édition

---
+++ src/Controller/AdController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Entity\Image;
+use App\Form\AnnonceType;
+use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo)
+    {
+
+        $ads = $repo->findAll();
+        
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+    
+    /**
+     * Permet de créer une annonce
+     * 
+     * @Route("/ads/new" , name="ads_create")
+     *
+     * @return Response
+     */
+
+    //ManagerRegistry $managerRegistry en remplacement de Objectmanager $manager -> https://stackoverflow.com/questions/59240233/symfony-4-cannot-autowire-argument-manager-of-it-references-interface-do
+    public function create(Request $request, ManagerRegistry $managerRegistry){
+        $ad = new Ad();
+
+        $form = $this->createForm(AnnonceType::class, $ad);
+
+        //handleRequest parcour la requête et extrait les informations du form
+        $form->handleRequest($request);
+
+        // vérifie le submit et permet de savoir si le formulaire est valide par raport aux rêgles en place
+        if($form->isSubmitted() && $form->isValid()){
+            foreach($ad->getImages() as $image) {
+                $em = $managerRegistry->getManager();
+                $image->setAd($ad);
+                $em->persist($image);
+            }
+
+            $em = $managerRegistry->getManager();
+            $em->persist($ad);
+            $em->flush();
+
+            $this->addFlash(
+                'success',
+                "L'article <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+
+            
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'édition
+     * 
+     * @Route("/ads/{slug}/edit", name="ads_edit")
+     *
+     * @return Response
+     */
+    public function edit(Ad $ad, Request $request, ManagerRegistry $managerRegistry){
+
+        $form = $this->createForm(AnnonceType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            foreach($ad->getImages() as $image) {
+                $em = $managerRegistry->getManager();
+                $image->setAd($ad);
+                $em->persist($image);
+            }
+
+            $em = $managerRegistry->getManager();
+            $em->persist($ad);
+            $em->flush();
+
+            $this->addFlash(
+                'success',
+                "Les modifications de l'article <strong>{$ad->getTitle()}</strong> ont bien été enregistrées !"
+            );
+
+            
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+        return $this->render('ad/edit.html.twig', [
+            'form' => $form->createView(),
+            'ad' => $ad
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une seule annonce
+     * 
+     * @Route("/ads/{slug}", name="ads_show")
+     *
+     * @return Response
+     */
+    public function show(Ad $ad){
+        return $this->render('ad/show.html.twig',[
+            'ad' => $ad
+        ]);
+    }
+
+}
---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ad;
+use App\Form\ImageType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class AnnonceType extends AbstractType
+{
+    /**
+     * Permet d'avoir la configuration de base d'un champ !
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @param array $options
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder, $options = []) {
+        return array_merge([
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ], $options);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, $this->getConfiguration("Titre", "Tapez le titre de votre article !"))
+            ->add('slug', TextType::class, $this->getconfiguration("Adresse web (automatique si non précisée)", "Tapez l'adresse web selon l'exemple suivant: mon-nom-d-article-! (automatique si non précisée)", [
+                'required' => false
+            ]))
+            ->add('coverImage', UrlType::class, $this->getConfiguration("URL de l'image principale", "Donnez l'adresse d'une image en lien avec l'article"))
+            ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Renseigner une description de l'article"))
+            ->add('content', TextareaType::class, $this->getConfiguration("Description détaillée", "Tapez une description complémentaire des informations techniques et du type de produit"))
+            ->add('price', MoneyType::class, $this->getConfiguration("Prix de l'article", "Indiquez le prix de l'article"))
+
+            ->add('images',CollectionType::class,[
+                'entry_type' => ImageType::class,
+                'allow_add' => true,
+                'allow_delete' => true
+                ]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ad::class,
+        ]);
+    }
+}
