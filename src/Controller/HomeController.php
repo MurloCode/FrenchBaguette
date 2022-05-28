12643da5c2af25ddead9dadcc1cd9caea08e036e
Corentin Marillier <marillier.io@gmail.com>
2022-05-28 13:10:23 +0200

    minor correction and removal of unnecessary comments

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,28 +3,21 @@
 namespace App\Controller;
 
 use App\Repository\AdRepository;
-use App\Repository\ProductRepository;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController extends Controller {
+class HomeController extends AbstractController {
    
     /**
      * @Route("/", name="homepage")
      */
     public function home(AdRepository $repo){
-        //permet d'afficher tous les articles
-        $ads = $repo->findAll();
-        //Permet d'afficher les derniers articles ajoutés au site depuis le AdRepository
         $lastProducts = $repo->findLastProductAdded();
 
         return $this->render('home.html.twig', [
-            'ads' => $ads,
             'lastProducts' => $lastProducts
         ]);
     }
 }
-
 
 ?>
